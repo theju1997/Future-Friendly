370a5b0584e487bfd0048064f654d43935433ab4
theju1997 <thejaswinitr@gmail.com>
2023-08-26 12:15:50 -0600

    student tracker

--- Phy-Fullstack/Frontend/Languages/TYPESCRIPT/Datatypes/Defaultdata-type/arrays/Object/reduce.js
+++
@@ -1,8 +0,0 @@
-var num = [3, 3, 2, 2, 9, 9, 1, 1];
-var uniqueNums = num.reduce(function (acc, current) {
-    if (!acc.includes(current)) {
-        acc.push(current);
-    }
-    return acc;
-}, []);
-console.log(uniqueNums);
