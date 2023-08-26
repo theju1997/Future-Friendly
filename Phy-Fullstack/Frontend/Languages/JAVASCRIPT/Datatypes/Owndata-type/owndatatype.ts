90bc8510361b6088d519501a6152b0d0eb19773b
theju1997 <thejaswinitr@gmail.com>
2023-08-26 12:18:33 -0600

    updated

---
+++ Phy-Fullstack/Frontend/Languages/JAVASCRIPT/Datatypes/Owndata-type/owndatatype.ts
@@ -0,0 +1,23 @@
+class Family{
+    familycount:Number
+    Father
+    Mother
+    Brother
+    Sister
+    Grandparents
+    constructor(){
+        this.familycount=6
+    }
+    withfamily(){
+        console.log("we can Spend our precious time")
+    }
+    withGrandparents(){
+        console.log("We can get experiences info")
+    }
+    
+
+}
+
+let Theju=new Family()
+Theju
+console.log(Theju)
