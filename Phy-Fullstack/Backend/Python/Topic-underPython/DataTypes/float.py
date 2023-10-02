9767e8ca7a72a54b66d9b198df9768cfd87b038f
Thejaswini TR <thejaswinitr@gmail.com>
2023-10-02 11:17:59 +0530

    python topics

--- Phy-Fullstack/Backend/Python/Topic-underPython/DataTypes/float.py
+++ Phy-Fullstack/Backend/Python/Topic-underPython/DataTypes/float.py
@@ -1,4 +1,7 @@
 a=22.24
 print (type(a))
+#<class 'float'>
 print (a)
+#22.24
 print(id(a))#Id will create automatically
+#2036644931536
--- Phy-Fullstack/Backend/Python/Topic-underPython/DataTypes/int.py
+++ Phy-Fullstack/Backend/Python/Topic-underPython/DataTypes/int.py
@@ -1,4 +1,8 @@
 a=100
 print (type(a))
+#<class 'int'>
 print (a)
+#100
 print(id(a))#Id will create automatically
+#140727520915336
+
