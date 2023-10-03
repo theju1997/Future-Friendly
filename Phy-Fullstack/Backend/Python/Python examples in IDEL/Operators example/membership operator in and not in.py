6ba3971e458fc7152c6f2ed21ad0f0c3b6f3c454
Thejaswini TR <thejaswinitr@gmail.com>
2023-10-03 22:44:53 +0530

    python topics

---
+++ Phy-Fullstack/Backend/Python/Python examples in IDEL/Operators example/membership operator in and not in.py
@@ -0,0 +1,15 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 'p' in 'python'
+True
+>>> 'y' in python
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    'y' in python
+NameError: name 'python' is not defined
+>>> 'y' in 'python'
+True
+>>> 'k' in 'python'
+False
+>>> 'k'not in'python'
+True
