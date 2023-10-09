9e17b2b826f7b24a0c82a33f658b9b4b20c13d24
Thejaswini TR <thejaswinitr@gmail.com>
2023-10-09 23:37:27 +0530

    Changes

---
+++ Phy-Fullstack/Backend/Python/Topic-underPython/Pattern program/prg2.py
@@ -0,0 +1,7 @@
+for i in range(1,5):
+        for j in range(1,7):
+                if i=1 or j=1 or j==6 or i==4:
+                        print('' , end=" ")
+                else:
+                    print('*',end=" ")
+        print()
