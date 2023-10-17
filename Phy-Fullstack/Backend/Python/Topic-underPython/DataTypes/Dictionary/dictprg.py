f7dd8f65dba7db9ecdf753af37704f19f1655375
Theju <thejaswinitr@gmail.com>
2023-10-17 23:06:39 +0530

    Dictionary

---
+++ Phy-Fullstack/Backend/Python/Topic-underPython/DataTypes/Dictionary/dictprg.py
@@ -0,0 +1,9 @@
+#1st way of declaring dictionary
+#Example
+x={'btech':2010,'mtech':2012,'phd':2023}
+print(x['btech'])
+
+#2nd way of declaring dictionary
+#Exampl
+y=dict(btech=2010,mtech=2012,phd=2023)
+print(y)
