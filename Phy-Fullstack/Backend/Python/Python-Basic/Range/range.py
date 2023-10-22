f3b1285a42b7a4d66b540d301e8f33630809cd29
Theju <thejaswinitr@gmail.com>
2023-10-22 23:09:08 +0530

    Range

---
+++ Phy-Fullstack/Backend/Python/Python-Basic/Range/range.py
@@ -0,0 +1,18 @@
+for i in range(11):
+    print(i, end='  ')
+                           #Ouput
+                           #0  1  2  3  4  5  6  7  8  9  10 
+for i in range(20,10,-2):
+    print(i, end='  ')                           
+                            #Ouput
+                           # 20  18  16  14  12
+
+for i in range(10,0,-1):
+    print(i, end='  ')                           
+                            #Ouput
+                           #10  9  8  7  6  5  4  3  2  1
+
+for i in range(20,10):
+    print(i, end='  ')                           
+                            #Ouput
+                           #No output python will not work backward direction(no -ve value)
