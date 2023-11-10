c2254d0fa89c5667650641f1f45d948020cc3f0b
Theju <thejaswinitr@gmail.com>
2023-11-10 21:12:13 +0530

    storing data in table

---
+++ Phy-Fullstack/Backend/Python/Framework/Projects/data/info/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import peoples
+# Create your views here.
+def display(request):
+    if request.method=='GET':
+        return render(request,'register.html')
+    else:
+        peoples(first_name = request.POST.get('first_name'),
+                last_name = request.POST.get('last_name'),
+                age =request.POST.get('age'),
+                contacts =request.POST.get('contacts')).save()
+                #email= request.POST.get('email')
+                #pw= request.POST.get('pw')
+    return render(request,'succes.html')
